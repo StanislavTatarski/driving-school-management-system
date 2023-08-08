1a248fc109597ae5d8b6c5c8f9e691d5cef0fb98
Stanislav Tatarski <stanislav@motokool.ee>
2023-08-08 20:05:48 +0300

    Updates: DrivingLesson and integrated Calendar.

--- src/main/java/ee/drivingschool/exception/Errors.java
+++ src/main/java/ee/drivingschool/exception/Errors.java
@@ -3,7 +3,8 @@ package ee.drivingschool.exception;
 public enum Errors {
     COURSE_NOT_FOUND(1),
     STUDENT_NOT_FOUND(2),
-    TEACHER_NOT_FOUND(3);
+    TEACHER_NOT_FOUND(3),
+    DRIVING_CARD_NOT_FOUND(4);
 
     private final int value;
 
